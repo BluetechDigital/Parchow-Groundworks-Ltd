fdbfe7cc5b08706b2c52087046311b51574772c6
Todd Owen Mpeli <toddowenmpeli@rocketmail.com>
2024-03-21 08:42:50 +0000

    Created a get all services slug fucntion and post type url DIR.

--- functions/graphql/Queries/GetAllPagesSlugs.ts
+++ functions/graphql/Queries/GetAllPagesSlugs.ts
@@ -32,3 +32,32 @@ export const getAllPagesSlugs = async (): Promise<ISlug> => {
 		throw new Error("Something went wrong trying to fetch all pages urls");
 	}
 };
+
+export const getAllServicesPagesSlugs = async (): Promise<ISlug> => {
+	try {
+		const content: DocumentNode = gql`
+			{
+				pageURLs: pages(
+					where: {status: PUBLISH, parentNotIn: "services"}
+					last: 100
+				) {
+					nodes {
+						slug
+						modified
+					}
+				}
+			}
+		`;
+
+		const response: any = await client.query({
+			query: content,
+		});
+
+		return response?.data?.pageURLs?.nodes;
+	} catch (error) {
+		console.log(error);
+		throw new Error(
+			"Something went wrong trying to fetch all services pages urls"
+		);
+	}
+};
